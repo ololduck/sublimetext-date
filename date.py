cc882dc6226c6e2796a6f1cc141847ef1e28b816
Paul Olliver <paul.ollivier@druil.net>
2012-05-03 21:21:01 +0200

    Corrected keymaps. Added Unnecessary project file.

--- date.py
+++ date.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 class DateCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit,
+		self.view.insert(
+			edit,
 			self.view.sel()[0].begin(),
 			datetime.now().strftime("%d/%m/%Y %H:%M")
 			)
