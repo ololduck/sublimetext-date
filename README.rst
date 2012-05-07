896a0eaa920ba629256c98316b1a887baf89890e
Paul Ollivier <paul.ollivier@druil.net>
2012-05-07 13:05:25 +0200

    Added Hour command, updated README

--- README.rst
+++ README.rst
@@ -4,3 +4,18 @@ sublimetext-date
 A plugin for Sublime Text 2 that inserts date and hour when f5 is pressed.
 
 Simple, yet, i kinda like it.
+
+Usage
+~~~~~
+
+::
+
+	F5: Prints date (DD/MM/AAAA) followed by hour (HH:MM)
+	shit+F5: prints only hour (HH:MM)
+
+
+Todo
+~~~~
+
+ * Act on multiple selection
+ * more options to control date format?
--- date.py
+++ date.py
@@ -2,9 +2,20 @@ import sublime, sublime_plugin
 from datetime import datetime
 
 class DateCommand(sublime_plugin.TextCommand):
+	"""Prints Date + H:M"""
 	def run(self, edit):
 		self.view.insert(
 			edit,
 			self.view.sel()[0].begin(),
 			datetime.now().strftime("%d/%m/%Y %H:%M")
 			)
+
+class HourCommand(sublime_plugin.TextCommand):
+	"""Prints only H:M"""
+	def run(self, edit):
+		self.view.insert(
+			edit,
+			self.view.sel()[0].begin(),
+			datetime.now().strftime("%H:%M")
+			)
+		
